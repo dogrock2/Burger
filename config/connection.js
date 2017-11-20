d4ed68ab20b10762680a6d97cc2b6c896fd1eec2
Alex <dogrock2@Alexanders-MacBook-Pro.local>
2017-11-19 19:45:40 -0500

    changed mySQL for JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,11 @@
 const mysql = require("mysql");
 
+let connection = '';
 
-let connection = mysql.createConnection({ 
+if(process.env.JAWSDB_URL)
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+else   
+    connection = mysql.createConnection({ 
     host: "localhost",
     port: 3306,
     user: 'alex',
